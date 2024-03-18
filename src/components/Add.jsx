ff377a63cac38c7e722192c4d7db1341c689171d
aleksandra <aleksandra.swigon91@gmail.com>
2024-03-18 21:20:32 +0100

    hotfix

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,18 +1,86 @@
-import { Fab, Tooltip } from "@mui/material";
+import {
+  Avatar,
+  Box,
+  Fab,
+  Modal,
+  Stack,
+  TextField,
+  Tooltip,
+  Typography,
+} from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
+import EmojiEmotionsIcon from "@mui/icons-material/EmojiEmotions";
+import ImageIcon from "@mui/icons-material/Image";
+import PhotoCameraFrontIcon from "@mui/icons-material/PhotoCameraFront";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import { useState } from "react";
+import styled from "@emotion/styled";
+
+const StyledModal = styled(Modal)({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+});
+
+const UserBox = styled(Box)({
+  display: "flex",
+  alignItems: "center",
+  gap: "10px",
+  marginBottom: "20px",
+});
+
 const Add = () => {
+  const [open, setOpen] = useState(false);
   return (
-    <Tooltip
-      sx={{
-        position: "fixed",
-        bottom: 20,
-        left: { xs: "calc(50% - 25px)", md: 30 },
-      }}
-    >
-      <Fab color='primary' aria-label='add'>
-        <AddIcon />
-      </Fab>
-    </Tooltip>
+    <>
+      <Tooltip
+        onClick={(e) => setOpen(true)}
+        sx={{
+          position: "fixed",
+          bottom: 20,
+          left: { xs: "calc(50% - 25px)", md: 30 },
+        }}
+      >
+        <Fab color='primary' aria-label='add'>
+          <AddIcon />
+        </Fab>
+      </Tooltip>
+      <StyledModal
+        open={open}
+        onClose={(e) => setOpen(false)}
+        aria-labelledby='modal-modal-title'
+        aria-describedby='modal-modal-description'
+      >
+        <Box width={400} height={280} bgcolor='white' p={3} borderRadius={5}>
+          <Typography variant='h6' color='gray' textAlign='center'>
+            Create post
+          </Typography>
+          <UserBox>
+            <Avatar
+              alt='Trevor Henderson'
+              src='https://mui.com/static/images/avatar/5.jpg'
+            />
+            <Typography fontWeight={500} variant='span'>
+              Trevor Henderson
+            </Typography>
+          </UserBox>
+          <TextField
+            sx={{ width: "100%" }}
+            id='standard-multiline-static'
+            multiline
+            rows={3}
+            placeholder="What's on your mind?"
+            variant='standard'
+          />
+          <Stack direction='row' gap={1} mt={2} mb={3}>
+            <EmojiEmotionsIcon color='primary' />
+            <ImageIcon color='secondary' />
+            <PhotoCameraFrontIcon color='success' />
+            <PersonAddIcon color='error' />
+          </Stack>
+        </Box>
+      </StyledModal>
+    </>
   );
 };
 
